80696e5f6f717f3ceef6f403905d4835d4fffff2
aj-abhinai <aayurpunarnava@gmail.com>
2025-09-06 09:46:25 +0530

    Quartz sync: Sep 6, 2025, 9:46 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -3,27 +3,20 @@ import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
 
-interface Options {
-  links: Record<string, string>
-}
 
-export default ((opts?: Options) => {
+export default (() => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
-    const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
+        <div class="footer-content">
+          <p class="right">
+            <small>
+              {i18n(cfg.locale).components.footer.createdWith}{" "}
+              <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+            </small>
+          </p>
+        </div>
       </footer>
     )
   }
